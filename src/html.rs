ff09d29c11b16db6b2ba9d9dee4f6f25510d2bb2
Esteban Borai <estebanborai@gmail.com>
2021-08-15 18:44:35 -0400

    feat: impl "remove_html_tags" utility function

--- src/html.rs
+++ src/html.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use scraper::{Html, Selector};
 
 pub fn first_inner_html(html: &Html, tag: &str) -> Option<String> {
@@ -36,4 +37,28 @@ pub fn find_link(html: &Html, rel: &str) -> Option<String> {
     }
 
     None
+}
+
+/// Removes HTML tags from the provided HTML text
+pub fn remove_html_tags(text: &str) -> String {
+    let re = Regex::new("<(.|\n)*?>").unwrap();
+    let res = re.replace_all(text, "");
+
+    res.to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::remove_html_tags;
+
+    #[test]
+    fn sanitizes_html_text() {
+        let html = "<html><body><p>Hello <b>World</b>!.<br /> This is our<sup>1st</sup> test on sanitization for HTML text</p><body></html>";
+        let sanitized = remove_html_tags(html);
+
+        assert_eq!(
+            sanitized,
+            "Hello World!. This is our1st test on sanitization for HTML text"
+        );
+    }
 }
